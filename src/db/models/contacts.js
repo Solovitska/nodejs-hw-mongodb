65686ef9a65ada8e7a35dab6effacef5e54d00e9
unknown <tanish@ua.i>
2024-06-27 00:52:11 +0300

    update

---
+++ src/db/models/contacts.js
@@ -0,0 +1,32 @@
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enam: ['personal', 'work', 'home'],
+      default: 'personal',
+    },
+  },
+  { timestamps: true },
+);
+
+export const Contact = model('contacts', contactsSchema);
---
+++ src/server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { env } from './env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: { target: 'pino-pretty' },
+    }),
+  );
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: res.statusCode,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(400).json({
+        message: 'Contact  not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: res.statusCode,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+/* eslint-disable no-unused-vars */
+  app.use('*', (req, res, next) => {
+    res.status(404).json({ message: 'Not found' });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
