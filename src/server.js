b9068ab10810176995a49dfa77db5a8ba871cc90
unknown <tanish@ua.i>
2024-07-11 17:14:35 +0300

    up

--- src/server.js
+++ src/server.js
@@ -1,31 +1,39 @@
 import express from 'express';
-import cors from 'cors';
 import pino from 'pino-http';
+import cors from 'cors';
 import { env } from './env.js';
-import router from './routers/contacts.js';
-import { errorHandler } from './middlewares/errorHandler.js';
+import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT', '3000'));
-
 export const setupServer = () => {
   const app = express();
 
-  app.use(cors());
-
   app.use(express.json());
+  app.use(cors());
 
   app.use(
     pino({
-      transport: { target: 'pino-pretty' },
+      transport: {
+        target: 'pino-pretty',
+      },
     }),
   );
 
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World!',
+    });
+  });
+
+  app.use(cookieParser());
   app.use(router);
 
-  app.use(errorHandler);
-
   app.use('*', notFoundHandler);
+
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
