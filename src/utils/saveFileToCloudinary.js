f34036129247e40f65f613f3f61a32e96a3b0dd8
unknown <tanish@ua.i>
2024-07-26 10:28:32 +0300

    up

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,16 +1,26 @@
-import cloudinary from 'cloudinary';
-
+import { v2 as cloudinary } from 'cloudinary';
 import { env } from './env.js';
 import { CLOUDINARY } from '../constants/index.js';
 
-cloudinary.v2.config({
-  secure: true,
-  cloud_name: env(CLOUDINARY.CLOUD_NAME),
-  api_key: env(CLOUDINARY.API_KEY),
-  api_secret: env(CLOUDINARY.API_SECRET),
-});
+if (process.env.ENABLE_CLOUDINARY === 'true') {
+  cloudinary.config({
+    secure: true,
+    cloud_name: env(CLOUDINARY.CLOUD_NAME),
+    api_key: env(CLOUDINARY.API_KEY),
+    api_secret: env(CLOUDINARY.API_SECRET),
+  });
+}
 
 export const saveFileToCloudinary = async (file) => {
-  const response = await cloudinary.v2.uploader.upload(file.path);
-  return response.secure_url;
+  if (process.env.ENABLE_CLOUDINARY !== 'true') {
+    throw new Error('Cloudinary integration is not enabled.');
+  }
+
+  try {
+    const response = await cloudinary.uploader.upload(file.path);
+    return response.secure_url;
+  } catch (error) {
+    console.error('Error uploading file to Cloudinary:', error);
+    throw new Error('File upload failed');
+  }
 };
